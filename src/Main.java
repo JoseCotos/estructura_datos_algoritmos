cc574016a4c2319d68118b6332c5983e029bce69
Jose-Cotos-Casa <josecotos@gmail.com>
2023-11-07 00:24:14 -0500

    Creación de la clase Feriado

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import entidades.Feriado;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+
+    }
+}
--- src/entidades/Feriado.java
+++ src/entidades/Feriado.java
@@ -1,2 +1,60 @@
-package entidades;public class Feriado {
+package entidades;
+
+public class Feriado {
+    private int idFeriado;
+    private int ferMes;
+    private int ferDiaSemana;
+    private String ferTipo;
+    private String ferDescripcion;
+
+    public Feriado(int idFeriado, int ferMes, int ferDiaSemana, String ferTipo, String ferDescripcion) {
+        this.idFeriado = idFeriado;
+        this.ferMes = ferMes;
+        this.ferDiaSemana = ferDiaSemana;
+        this.ferTipo = ferTipo;
+        this.ferDescripcion = ferDescripcion;
+    }
+
+    public Feriado() {
+    }
+
+    public int getIdFeriado() {
+        return idFeriado;
+    }
+
+    public void setIdFeriado(int idFeriado) {
+        this.idFeriado = idFeriado;
+    }
+
+    public int getFerMes() {
+        return ferMes;
+    }
+
+    public void setFerMes(int ferMes) {
+        this.ferMes = ferMes;
+    }
+
+    public int getFerDiaSemana() {
+        return ferDiaSemana;
+    }
+
+    public void setFerDiaSemana(int ferDiaSemana) {
+        this.ferDiaSemana = ferDiaSemana;
+    }
+
+    public String getFerTipo() {
+        return ferTipo;
+    }
+
+    public void setFerTipo(String ferTipo) {
+        this.ferTipo = ferTipo;
+    }
+
+    public String getFerDescripcion() {
+        return ferDescripcion;
+    }
+
+    public void setFerDescripcion(String ferDescripcion) {
+        this.ferDescripcion = ferDescripcion;
+    }
 }
