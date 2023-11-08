cccda8b366fbe0a3cda6cae38943e048b9892871
Jose-Cotos-Casa <josecotos@gmail.com>
2023-11-07 23:59:19 -0500

    trabajando con la versión openjdk-21 v 21.0.1

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
 
+        //Para pruebas de datos cargado
         Feriado feriado = new Feriado();
         feriado.llenarDatosIniciales();
         LinkedList<Feriado> colFeriado = feriado.getColFeriado();
